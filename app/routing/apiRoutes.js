015d09efdb51971f5c6d6d87e7c346c603095566
Pwneill <phelan.oneill@gmail.com>
2018-12-19 19:35:23 -0600

    fixed post in htmlRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,23 @@
 var userData = require("../data/friends.js");
 
+var bodyParser = require("body-parser");
+
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(userData);
-    console.log(UserData);
+    console.log(userData);
   });
 
-  app.post("/api/friends", function(req, res) {
-      
-  })
-};
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body);
+    userData.push(req.body);
+    res.json(true);
+    // console.log(userData)
+  });
+};
