f15a810a4f45fbe474b1baef58dc7454382ea6dd
Pwneill <phelan.oneill@gmail.com>
2018-12-21 13:19:14 -0600

    finished logic for finding the best match and added data for each politician

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,52 @@
 var userData = [
   {
     name: "Amy Klobuchar",
-    photo: app / images / amyklobuchar.jpg,
+    // photo: app / images / amyklobuchar.jpg,
     description: "foo",
-    scores: [1, 2]
+    scores: [3, 2, 3, 2, 3, 4, 4, 2, 3],
+    scoreDiff: 0
   },
-  {
-    name: "Beto O'Rourke",
-    photo: app / images / betoorourke.jpg,
-    description: "foo",
-    scores: [1, 2]
-  },
+  // {
+  //   name: "Beto O'Rourke",
+  //   photo: app / images / betoorourke.jpg,
+  //   description: "foo",
+  //   scores: [1, 2]
+  // },
   {
     name: "Joe Biden",
-    photo: app / images / joebiden.jpg,
+    // photo: app / images / joebiden.jpg,
     description: "foo",
-    scores: [1, 2]
+    scores: [5, 2, 3, 1, 5, 4, 4, 2, 5, 2],
+    scoreDiff: 0
   },
   {
     name: "Cory Booker",
-    photo: app / images / coreybooker.jpg,
+    // photo: app / images / coreybooker.jpg,
     description: "foo",
-    scores: [1, 2]
+    scores: [2, 5, 3, 5, 4, 4, 2, 3, 5, 5],
+    scoreDiff: 0
   },
   {
     name: "Kamala Harris",
-    photo: app / images / kamalaharris.jpg,
+    // photo: app / images / kamalaharris.jpg,
     description: "foo",
-    scores: [1, 2]
+    scores: [2, 3, 3, 1, 3, 5, 3, 3, 5, 3],
+    scoreDiff: 0
   },
   {
     name: "Elizabeth Warren",
-    photo: app / images / elizabethwarren.jpg,
+    // photo: app / images / elizabethwarren.jpg,
     description: "foo",
-    scores: [1, 2]
-  },{
-    "name":"Bernie Sanders",
-    "photo": app/images/berniesanders.jpg,
-    "description": "foo",
-    "scores": [
-      1,
-      2
-    ]},
+    scores: [4, 5, 1, 2, 3, 5, 5, 5, 5, 5],
+    scoreDiff: 0
+  },
+  {
+    name: "Bernie Sanders",
+    // photo: app / images / berniesanders.jpg,
+    description: "foo",
+    scores: [4, 3, 1, 5, 1, 5, 5, 5, 4, 1],
+    scoreDiff: 0
+  }
 ];
 
 module.exports = userData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,9 +2,8 @@ var userData = require("../data/friends.js");
 
 var bodyParser = require("body-parser");
 
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-
 module.exports = function(app) {
+  // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
   app.get("/api/friends", function(req, res) {
     res.json(userData);
     console.log(userData);
@@ -15,10 +14,40 @@ module.exports = function(app) {
 
   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post("/api/friends", function(req, res) {
-    console.log("this is running")
-    // console.log(req.body);
-    // userData.push(req.body);
-    // res.json(true);
-    // console.log(userData)
+    var user = req.body.scores;
+    var userScore = [];
+    user.forEach(function(score) {
+      userScore.push(parseInt(score));
+    });
+
+    var bestMatch = {};
+
+    for (j = 0; j < userData.length; j++) {
+      var pol = userData[j];
+      var scoreDiff = 0;
+
+      for (i = 0; i < 9; i++) {
+        scoreDiff += Math.abs(pol.scores[i] - userScore[i]);
+        // console.log(scoreDiff);
+      }
+
+      if (
+        scoreDiff < bestMatch.scoreDiff ||
+        bestMatch.scoreDiff === undefined
+      ) {
+        bestMatch = pol;
+        bestMatch.scoreDiff = scoreDiff;
+      } else {
+      }
+      // console.log(scoreDiff)
+      // console.log("\n----------------------------\n")
+      // console.log(req.body);
+      // userData.push(req.body);
+      // res.json(true);
+      // console.log(userData)
+    }
+
+    console.log(bestMatch)
+    res.json(bestMatch)
   });
 };
