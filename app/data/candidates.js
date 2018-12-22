6e587bd8cc6bdbb413d6624f180e9eedda1a4e18
Pwneill <phelan.oneill@gmail.com>
2018-12-22 10:52:44 -0800

    added POST for voters

--- app/data/candidates.js
+++ app/data/candidates.js
@@ -1,6 +1,7 @@
 var userData = [
   {
     name: "Amy Klobuchar",
+    photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/amyklobuchar.jpg",
     // photo: app / images / amyklobuchar.jpg,
     description: "foo",
     scores: [3, 2, 3, 3, 2, 3, 4, 4, 2, 3],
@@ -13,31 +14,31 @@ var userData = [
   // },
   {
     name: "Joe Biden",
-    // photo: app / images / joebiden.jpg,
+    photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/joebiden.jpg",
     description: "foo",
     scores: [5, 2, 3, 1, 5, 4, 4, 2, 5, 2],
   },
   {
     name: "Cory Booker",
-    // photo: app / images / coreybooker.jpg,
+    photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/amyklobuchar.jpg",
     description: "foo",
     scores: [2, 5, 3, 5, 4, 4, 2, 3, 5, 5],
   },
   {
     name: "Kamala Harris",
-    // photo: app / images / kamalaharris.jpg,
+    photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/kamalaharris.jpg",
     description: "foo",
     scores: [2, 3, 3, 1, 3, 5, 3, 3, 5, 3],
   },
   {
     name: "Elizabeth Warren",
-    // photo: app / images / elizabethwarren.jpg,
+    photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/elizabethwarren.jpg",
     description: "foo",
     scores: [4, 5, 1, 2, 3, 5, 5, 5, 5, 5],
   },
   {
     name: "Bernie Sanders",
-    // photo: app / images / berniesanders.jpg,
+    photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/berniesanders.jpg",
     description: "foo",
     scores: [4, 3, 1, 5, 1, 5, 5, 5, 4, 1],
   }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,39 @@
-var userData = require("../data/friends.js");
+var candidates = require("../data/candidates.js");
+
+var voters = require("../data/voters.js");
 
 var bodyParser = require("body-parser");
 
 module.exports = function(app) {
-  // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-  app.get("/api/friends", function(req, res) {
-    res.json(userData);
-    console.log(userData);
+  // A GET route with the url /api/candidates. This will be used to display a JSON of all possible candidates.
+  app.get("/api/candidates", function(req, res) {
+    res.json(candidates);
+    // console.log(candidates);
+  });
+
+  // A GET route with the url /api/voters. This will be used to display a JSON of all possible voters.
+  app.get("/api/voters", function(req, res) {
+    res.json(voters);
+    // console.log(candidates);
   });
 
   app.use(bodyParser.json()); // for parsing application/json
   app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-  app.post("/api/friends", function(req, res) {
-    var user = req.body.scores;
+  app.post("/api/voters", function(req, res) {
+    var user = req.body;
+    var scores = user.scores;
+    voters.push(user)
     var userScore = [];
-    user.forEach(function(score) {
+    scores.forEach(function(score) {
       userScore.push(parseInt(score));
     });
 
     var bestMatch = {};
 
-    for (j = 0; j < userData.length; j++) {
-      var pol = userData[j];
+    for (j = 0; j < candidates.length; j++) {
+      var pol = candidates[j];
       var scoreDiff = 0;
 
       for (i = 0; i < 10; i++) {
@@ -36,11 +46,13 @@ module.exports = function(app) {
       ) {
         bestMatch = {
           name: pol.name,
+          image: pol.photo,
           description: pol.description,
           scores: pol.scores,
           scoreDiff: scoreDiff
-      }} 
+        };
+      }
     }
-    res.json(bestMatch)
+    res.json(bestMatch);
   });
 };
