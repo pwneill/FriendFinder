11214d4519240754c2ab1522188340cff5cea1b4
Pwneill <phelan.oneill@gmail.com>
2018-12-22 20:21:43 -0800

    fun with heroku not

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
