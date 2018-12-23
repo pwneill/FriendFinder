552453a1ee4868d40f35621783b3af91abe977c5
Pwneill <phelan.oneill@gmail.com>
2018-12-23 11:20:25 -0800

    fixing some issues with image paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,36 +4,37 @@ var voters = require("../data/voters.js");
 
 var bodyParser = require("body-parser");
 
-  module.exports = function(app) {
-    
-    app.use(bodyParser.json()); // for parsing application/json
-    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+module.exports = function(app) {
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-    // A GET route with the url /api/candidates. This will be used to display a JSON of all possible candidates.
-    app.get("/api/candidates", function(req, res) {
-      res.json(candidates);
-      // console.log(candidates);
-    });
+  // A GET route with the url /api/candidates. This will be used to display a JSON of all possible candidates.
+  app.get("/api/candidates", function(req, res) {
+    res.json(candidates);
+    // console.log(candidates);
+  });
 
-    app.get("/api/voters/:candidate", function(req, res) {
+  //A GET route with the url /api/voters/:candidate. This will be used to display a JSON of all voters matched with the possible candidate.
+  app.get("/api/voters/:candidate", function(req, res) {
+    var similarVoters = [];
 
-      var similarVoters = [];
-
-      for (i = 0; i < voters.length; i++) {
-        var candidate = voters[i].matchedPolitician.replace(/\s/g, '').toLowerCase()
-        console.log(candidate)
-        if (candidate === req.params.candidate) {
-          similarVoters.push(voters[i])
-        }
+    for (i = 0; i < voters.length; i++) {
+      var candidate = voters[i].matchedPolitician
+        .replace(/\s/g, "")
+        .toLowerCase();
+      console.log(candidate);
+      if (candidate === req.params.candidate) {
+        similarVoters.push(voters[i]);
       }
-      res.json(similarVoters)
-    })
-  
-    // A GET route with the url /api/voters. This will be used to display a JSON of all possible voters.
-    app.get("/api/voters", function(req, res) {
-      res.json(voters);
-      // console.log(candidates);
-    });
+    }
+    res.json(similarVoters);
+  });
+
+  // A GET route with the url /api/voters. This will be used to display a JSON of all possible voters.
+  app.get("/api/voters", function(req, res) {
+    res.json(voters);
+    // console.log(candidates);
+  });
   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post("/api/voters", function(req, res) {
     var user = req.body;
@@ -71,11 +72,11 @@ var bodyParser = require("body-parser");
       photo: user.photo,
       description: user.description,
       scores: user.scores,
-      matchedPolitician: bestMatch.name 
-    }
-    
-    console.log(newVoter)
-    voters.push(newVoter)
+      matchedPolitician: bestMatch.name
+    };
+
+    console.log(newVoter);
+    voters.push(newVoter);
     res.json(bestMatch);
   });
 };
