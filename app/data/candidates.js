57fd4cda8424e26bd318f1e7c7f5605a80c32244
Pwneill <phelan.oneill@gmail.com>
2018-12-22 20:53:53 -0800

    added politicain bios

--- app/data/candidates.js
+++ app/data/candidates.js
@@ -2,44 +2,43 @@ var userData = [
   {
     name: "Amy Klobuchar",
     photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/amyklobuchar.jpg",
-    // photo: app / images / amyklobuchar.jpg,
-    description: "foo",
+    description: "Amy Klobuchar is a US Senator from Minnesota and former Hennepin County Attorney. Born and raised in Minnesota, Klobuchar is known for her pragmatism and dedication to finding common sense solutions to consumer safety and other bipartisan issues",
     scores: [3, 2, 3, 3, 2, 3, 4, 4, 2, 3],
   },
-  // {
-  //   name: "Beto O'Rourke",
-  //   photo: app / images / betoorourke.jpg,
-  //   description: "foo",
-  //   scores: [1, 2]
-  // },
+  {
+    name: "Beto O'Rourke",
+    photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/amyklobuchar.jpg",
+    description: "Beto O'Rourke is a US Congressman from El Paso, Texas. At 37 years of age, Beto is the youngest candidate considering a run for president. Beto became famous in 2018 running against Ted Cruz for US Senate and ran on a platform that included Medicare for All and pledging not to take corporate PAC money.",
+    scores: [1, 5, 4, 4, 3, 3, 4, 3, 4, 4]
+  },
   {
     name: "Joe Biden",
     photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/joebiden.jpg",
-    description: "foo",
+    description: "Joe Biden is a former Vice President, serving under Barack Obama. Biden is best known for the humorous portrayals of him in the satirical news source The Onion. In the past, Joe Biden has campaigned on manufacturing jobs and union issues, but has been more conservative on financial issues.",
     scores: [5, 2, 3, 1, 5, 4, 4, 2, 5, 2],
   },
   {
     name: "Cory Booker",
     photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/amyklobuchar.jpg",
-    description: "foo",
+    description: "Cory Booker is a US Senator from New Jersey and is formerly the mayor of Newark. Cory Booker was one of the first politicains to rely on social media. Cory Booker has been a proponent of criminal justice reform, and of more moderate economic regulations",
     scores: [2, 5, 3, 5, 4, 4, 2, 3, 5, 5],
   },
   {
     name: "Kamala Harris",
     photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/kamalaharris.jpg",
-    description: "foo",
+    description: "Kamala Harris is a US Senator from California. Serving her first term, Harris has stood out for her aggressive questioning of Trump Appointees and been a leading advocate for the regulation of social media platforms like Facebook",
     scores: [2, 3, 3, 1, 3, 5, 3, 3, 5, 3],
   },
   {
     name: "Elizabeth Warren",
     photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/elizabethwarren.jpg",
-    description: "foo",
+    description: "Elizabeth Warren is a US Senator from Massachusettes. Warren is most notable for her academic work that formed the basis for many major economic reforms implemented under President Obama. She's been a leading advocate for consumer protections and financial regulations.",
     scores: [4, 5, 1, 2, 3, 5, 5, 5, 5, 5],
   },
   {
     name: "Bernie Sanders",
     photo: "https://raw.githubusercontent.com/pwneill/FriendFinder/master/app/images/berniesanders.jpg",
-    description: "foo",
+    description: "Bernie Sanders is a US Senator from Vermont. He became well known for his bold, progressive run for president in 2016. Sanders has been a champion of Medicare for All, a higher minimum wage and breaking up the banking industry. ",
     scores: [4, 3, 1, 5, 1, 5, 5, 5, 4, 1],
   }
 ];
